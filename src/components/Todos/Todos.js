5bde89d7273996ccc27d58e24f73bc8499523c1e
Wajid Ali <wajidalii.me@gmail.com>
2022-11-29 19:47:36 +0500

    initial commit

---
+++ src/components/Todos/Todos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Todo from './Todo'
+function Todos() {
+    return (
+        <div>
+            <Todo />
+            <Todo />
+        </div>
+    )
+}
+
+export default Todos
